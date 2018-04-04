ef92a4cb335b8de32cff10b98a0ef6bea2fd043c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-04-05 01:16:38 +0200

    Fix shuffleCards function

--- js/app.js
+++ js/app.js
@@ -1,52 +1,18 @@
-const card = document.querySelectorAll('.card');
+const card = document.querySelectorAll('.front');
 const cards = [...card];
 
 init();
 
 function shuffleCards() {
-
-    for (let i = 0; i < cards.length; i++) {
-        let random = Math.floor(Math.random() * (cards.length - 1));
-
-        let cards = cards[i];
-        cards[i] = cards[random];
-        cards[random] =  cards;
-      }
-    // return array;
+  for (let i = 0; i < cards.length; i++) {
+      let random = Math.floor(Math.random() * (cards.length - 1));
+      let a = cards[i].innerText;
+      cards[i].innerText = cards[random].innerText;
+      cards[random].innerText =  a;
+    }
 }
+
 function init() {
   shuffleCards();
 }
-
-
-// const cards = ['&alpha;', '&alpha;', '&beta;', '&beta;', '&gamma;', '&gamma;', '&delta;', '&delta;', '&zeta;', '&zeta;', '&eta;', '&eta;', '&lambda;', '&lambda;', '&mu;', '&mu;'];
-// const front = document.querySelectorAll('.front');
-// // const cards = ['alpha', 'alpha', 'beta', 'beta', 'gamma', 'gamma', 'delta', 'delta', 'zeta', 'zeta', 'eta', 'eta', 'lambda', 'lambda', 'mi', 'mi'];
-// function shuffleCards(array) {
-//
-//   let currentIndex = cards.length;
-//   let temporaryValue;
-//   let randomIndex;
-//
-//   while (currentIndex !== 0) {
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex -= 1;
-//       temporaryValue = array[currentIndex];
-//       array[currentIndex] = array[randomIndex];
-//       array[randomIndex] = temporaryValue;
-//   };
-//
-//   return array;
-//
-// };
-//
-// function createBoard() {
-// 	for (card of cards) {
-//     const newDiv = document.createElement('div');
-//     newDiv.className = 'front';
-//     newDiv.innerHTML = cards[card];
-//     document.getElementsByClassName('card').appendChild(newDiv);
-//   }
-//
-// }
-// shuffleCards(cards);
+window.addEventListener("click", init);
