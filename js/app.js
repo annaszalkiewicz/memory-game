a4ca249e1a8d40b898db3bfc30582bbc570d694a
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-04-30 23:51:08 +0200

    Add background animation

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ function startGame() {
 
 	shuffleCards();
 	showTime();
-	
+
 }
 
 // Function to shuffle cards randomly
@@ -45,7 +45,7 @@ function shuffleCards() {
 		let random = Math.floor(Math.random() * (cards.length - 1));
 		let a = cards[i].innerText;
 		cards[i].innerText = cards[random].innerText;
-		cards[random].innerText =  a;
+		cards[random].innerText = a;
 	}
 }
 
@@ -65,14 +65,14 @@ function flipCard(event) {
 
 function matched() {
 
-	setTimeout(function(){
-		openCards.forEach(function(card){
+	setTimeout(function () {
+		openCards.forEach(function (card) {
 			card.classList.add('match');
 			matchedCards.push(card);
 
 			if (matchedCards.length === 16) {
 				endGame();
-			}  
+			}
 
 		});
 		openCards = [];
@@ -83,9 +83,9 @@ function matched() {
 // Function to run when 2 cards don't match
 
 function unmatched() {
- 
-	setTimeout(function(){
-		openCards.forEach(function(card){
+
+	setTimeout(function () {
+		openCards.forEach(function (card) {
 			card.classList.remove('flip');
 		});
 		openCards = [];
@@ -97,22 +97,22 @@ function unmatched() {
 function checkCards() {
 
 	for (let openCard of openCards) {
-		if ((openCards.length === 2)&&(openCards[0].innerText === openCards[1].innerText)) {
+		if ((openCards.length === 2) && (openCards[0].innerText === openCards[1].innerText)) {
 			moveCounter();
 			matched();
 
 		}
-		else if ((openCards.length === 2)&&(openCards[0].innerText !== openCards[1].innerText)) {
+		else if ((openCards.length === 2) && (openCards[0].innerText !== openCards[1].innerText)) {
 			moveCounter();
 			unmatched();
 		}
 	}
-  
+
 }
 function moveCounter() {
 
-	moves+=1/2;
-	movesCount.innerText = moves; 
+	moves += 1 / 2;
+	movesCount.innerText = moves;
 	checkStars();
 
 }
@@ -123,11 +123,11 @@ function checkStars() {
 	const twoStars = document.getElementById('two-stars');
 	const oneStar = document.getElementById('one-star');
 
-	if ((moves > 15)&&(moves < 25)) {
+	if ((moves > 15) && (moves < 25)) {
 		threeStars.style.color = '#000';
 		stars = '2 stars';
-	} 
-	else if ((moves > 25)&&(moves < 35)) {
+	}
+	else if ((moves > 25) && (moves < 35)) {
 		twoStars.style.color = '#000';
 		stars = '1 star';
 	}
@@ -154,18 +154,18 @@ function gameOver() {
 function showTime() {
 
 	time = setInterval(getTime, 1000);
-  
+
 	function getTime() {
 		timer.innerHTML = `${minutes}m ${seconds}s`;
-		seconds ++;
+		seconds++;
 
 		if (seconds === 60) {
 			minutes += 1;
 			seconds = 0;
 		}
-    
+
 	}
-  
+
 }
 
 //Function to stop timer
@@ -182,7 +182,7 @@ function endGame() {
 		endModal.classList.add('show');
 		message();
 	}, 1000);
-  
+
 }
 
 // MODAL ON THE START OF GAME
@@ -212,6 +212,14 @@ function message() {
 	endGameMessage.innerHTML = `You finished game in ${timer.innerHTML} and ${moves} moves. You gained ${stars}.`;
 }
 
+// for (gameCard of gameCards) {
+// 	gameCard.addEventListener('keypress', function (e) {
+// 		if (e.key === 13) {
+// 			flipCard();
+// 		}
+// 	});
+// }
+
 // EVENT LISTENERS
 
 // Reload game on button click
@@ -223,9 +231,9 @@ for (gameCard of gameCards) {
 }
 
 // Flip card event when using keyboard
-for (gameCard of gameCards) {
-	gameCard.addEventListener('keypress', flipCard);
-}
+
+
+
 
 // Event to close modal on click close button
 closeButton.addEventListener('click', closeStartModal);
@@ -237,3 +245,51 @@ window.addEventListener('click', closeEndModal);
 
 // Event to start game on click button
 startButton.addEventListener('click', startGame);
+
+// ANIMATED BACKGROUND
+
+const letters = {
+
+	// Array of letters
+
+	chars: ['&alpha;', '&Alpha;', '&beta;', '&Beta;', '&gamma;', '&Gamma;', '&delta;', '&Delta;', '&epsilon;', '&Epsilon;', '&zeta;', '&Zeta;', '&eta;', '&Eta;', '&theta;', '&Theta;', '&iota;', '&Iota;', '&kappa;', '&Kappa;', '&lambda;', '&Lambda;', '&mu;', '&Mu;', '&nu;', '&Nu;', '&xi;', '&Xi;', '&omicron;', '&Omicron;', '&pi;', '&Pi;', '&rho;', '&Rho;', '&sigma;', '&Sigma;', '&sigmaf;', '&tau;', '&Tau;', '&Upsilon;', '&upsilon;', '&phi;', '&Phi;', '&Chi;', '&chi;', '&psi;', '&Psi;', '&omega;', '&Omega;'],
+
+	// Function to create each letter every 300ms
+
+	repeat: function () {
+		window.setInterval(letters.create, 200);
+	},
+
+	// Function to create letter and place it in DOM structure
+
+	create: function () {
+		letters.container = document.getElementById('animation');
+		character = letters.chars[Math.floor(Math.random() * letters.chars.length)];
+		newSpan = document.createElement('span');
+		letters.container.appendChild(newSpan);
+		newSpan.innerHTML = character;
+		letters.animate(newSpan);
+	},
+
+	// Function setting to style each letter
+
+	animate: function (newSpan) {
+		let pos = Math.random() * 100;
+		let size = Math.random() * (300 - 20) + 20;
+		let duration = Math.random() * (20 - 5) + 5;
+		const color = ['#9e2021;', '#155f60;', '#6c943e;', '#1e7f3f;'];
+		let randomColor = color[Math.floor(Math.random() * color.length)];
+		newSpan.style.cssText = `margin-left: ${pos}vw; font-size: ${size}px; animation-duration: ${duration}s; color: ${randomColor}`;
+		window.setTimeout(letters.delete, duration * 1000, newSpan);
+	},
+
+	// Function to delete each letter when animation is finished
+
+	delete: function (newSpan) {
+		letters.container.removeChild(newSpan);
+	}
+};
+
+// Event listener to start letters animation when document is loaded
+
+document.addEventListener('DOMContentLoaded', letters.repeat);
